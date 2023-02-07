8cadb542f82b766c7744517ce8395b272854dd67
Samuel Calderon <samuel.calderon@uarm.pe>
2023-02-07 16:54:25 -0500

    first

---
+++ src/Accordion.tsx
@@ -0,0 +1,121 @@
+import React, { Component } from 'react'
+
+export class Accordion extends Component {
+    constructor(props) {
+        super(props)
+      
+        this.state = {
+           
+        }
+    }
+    
+    textInput(inputId: string, label: string, placeholder: string = "", helpText: string = "") {
+      const helpTextWrapper = helpText.length > 0 ? <small id={inputId + "helpId"} className="form-text text-muted">{helpText}</small>:""
+      return(
+        <div className="mb-3">
+          <label htmlFor={inputId} className="form-label">
+            <span className="badge px-2 me-2 vi-ciudad" style={{height: "0.8rem", backgroundColor: "red"}}> </span>
+            {label}
+          </label>
+          <input type="text"
+            className="form-control" name={inputId} id={inputId} aria-describedby={inputId + "helpId"} placeholder={placeholder} />
+          {helpTextWrapper}
+        </div>
+      )
+    }
+
+    numericInput(inputId: string, label: string, value: number = 0, helpText: string = "") {
+      const helpTextWrapper = helpText.length > 0 ? <small id={inputId + "helpId"} className="form-text text-muted">{helpText}</small>:""
+      return(
+        <div className="mb-3">
+          <label htmlFor={inputId} className="form-label">
+            {label}
+          </label>
+          <input 
+              type="number" 
+              id={inputId} 
+              className="form-control" 
+              name={inputId} 
+              aria-describedby={inputId+"helpId"} 
+              placeholder = "0"
+              min = {0}
+              max ={99}
+          />
+          {helpTextWrapper}
+        </div>
+      )
+    }
+
+    selectInput() {
+      return(
+        <div className="mb-3">
+          <label htmlFor="oportunidad" className="form-label">
+            Oportunidad
+          </label>
+          <select className="form-select" name="" id="oportunidad" defaultValue="al inicio">
+            <option value="" >al inicio</option>
+            <option value="">periódica</option>
+            <option value="">al cesar</option>
+          </select>
+        </div>
+      )
+    }
+
+    render() {
+        return (
+        <div className="accordion" id="accordionInputs">
+          
+          {/* Welcome */}
+          <div className="accordion-item">
+            <h2 className="accordion-header" id="welcome-heading">
+              <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#welcome-collapse" aria-expanded="true" aria-controls="welcome-collapse">
+                <h2>Aplicativo</h2>
+              </button>
+            </h2>
+            <div id="welcome-collapse" className="accordion-collapse collapse show" aria-labelledby="welcome-heading" data-bs-parent="#accordionInputs">
+              <div className="accordion-body">
+                This is the first item's accordion body.
+              </div>
+            </div>
+          </div>
+          
+          {/* Inputs */}
+          <div className="accordion-item">
+            <h2 className="accordion-header" id="inputs-heading">
+              <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#inputs-collapse" aria-expanded="false" aria-controls="inputs-collapse">
+              <i className="bi bi-input-cursor-text me-2"></i>Inputs
+              </button>
+            </h2>
+            <div id="inputs-collapse" className="accordion-collapse collapse" aria-labelledby="inputs-heading" data-bs-parent="#accordionInputs">
+              <div className="accordion-body">
+                {this.textInput("ciudad", "Ciudad de remisión")}
+                {this.textInput("remitente", "Nombre de remitente")}
+                {this.textInput("dni", "N° de DNI", "Ejemplo: 07654321")}
+                {this.numericInput("folios", "N° de folios")}
+                {this.selectInput()}
+                {this.textInput("email", "Email")}
+                {this.textInput("telefono", "N° de teléfono", "Ejemplo: 987654321")}
+              </div>
+            </div>
+          </div>
+
+          {/* Info */}
+          <div className="accordion-item">
+            <h2 className="accordion-header" id="info-heading">
+              <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#info-collapse" aria-expanded="false" aria-controls="info-collapse">
+              <i className="bi bi-info-circle me-2"></i>Info
+              </button>
+            </h2>
+            <div id="info-collapse" className="accordion-collapse collapse" aria-labelledby="info-heading" data-bs-parent="#accordionInputs">
+              <div className="accordion-body">
+                This is the first item's accordion body.
+              </div>
+            </div>
+          </div>
+          
+        </div>
+        )
+    }
+}
+
+export default Accordion
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,112 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
+import { Document, Packer, Paragraph, TextRun, AlignmentType } from "docx";
+import { saveAs } from "file-saver";
+import Accordion from './Accordion'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export class App extends Component {
+  constructor(props: any) {
+    super(props)
+  
+    this.state = {
+       
+    }
+    this.renderDocx = this.renderDocx.bind(this)
+  }
+
+  // Documents contain sections, you can have multiple sections per document, go here to learn more about sections
+// This simple example will only contain one section
+doc = new Document({
+  sections: [
+      {
+          properties: {},
+          children: [
+              new Paragraph({
+                  text: "Lima, 6 de febrero de 2023",
+                  alignment: AlignmentType.RIGHT,
+              }),
+              new Paragraph({
+                text: "CARTA N° 01-JP",
+                alignment: AlignmentType.LEFT
+              }),
+              new Paragraph({
+                text: "Señor",
+                alignment: AlignmentType.LEFT
+              }),
+              new Paragraph({
+                children: [
+                  new TextRun({
+                    text: "Subgerente de Gestión de Declaraciones Juradas",
+                    bold: true
+                  })
+                ],
+                alignment: AlignmentType.LEFT
+              }),
+              new Paragraph({
+                text: "Contraloría General de la República Jr. Camilo Carrillo 114",
+                alignment: AlignmentType.LEFT
+              }),
+              new Paragraph({
+                children: [
+                  new TextRun({
+                    text: "Jesús María /Lima /Lima",
+                    bold: true
+                  })
+                ],
+                alignment: AlignmentType.LEFT
+              }),
+              new Paragraph({
+                text: "Yo, Juan Perez, identificado con DNI fdsa, me dirijo a usted, en el marco de la normativa de la referencia, a fin de efectuar la remisión en original, en 1 folios, y en sobre cerrado adjunto al presente, de mi DJI correspondiente al ejercicio presupuestal 2023 y de oportunidad de presentación al inicio .",
+                alignment: AlignmentType.JUSTIFIED
+              }),
+              new Paragraph({
+                text: "Cualquier coordinación al respecto, sírvase comunicarse al correo electrónico fds o al teléfono sfd.",
+                alignment: AlignmentType.JUSTIFIED
+              }),
+              new Paragraph({
+                text: "Atentamente,",
+                alignment: AlignmentType.LEFT
+              }),
+              new Paragraph({
+                text: "________________",
+                alignment: AlignmentType.CENTER
+              }),
+              new Paragraph({
+                text: "Juan Perez",
+                alignment: AlignmentType.CENTER
+              }),
+          ],
+      },
+  ],
+});
+
+
+  renderDocx() {
+    // Used to export the file into a .docx file
+    Packer.toBlob(this.doc).then((blob) => {
+      // saveAs from FileSaver will download the file
+      saveAs(blob, "example.docx");
+  });
+  
+  }
+
+  render() {
+    return (
+      <div className="">
+        <div className="container-fluid bg-danger p-2">
+          <div className="row">
+            <div className="col-md-4 col-sm-12 bg-info p-2">
+              <Accordion />
+            </div>
+            <div className="col-md-8 col-sm-12 bg-warning p-2">
+              <input name="" id="" className="btn btn-primary" type="button" value="Button" onClick={this.renderDocx}/>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
